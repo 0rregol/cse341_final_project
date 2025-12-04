fe63291c1d537b991f31117f4881cbb7443ab0fc
0rregol <orr2404@byui.edu>
2025-12-04 16:55:33 -0300

    251204_02

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ const port = process.env.PORT || 3000;
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const cors = require('cors');
 
 app
   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use(cors()) 
   .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
   .use('/', require('./routes'));
 
 mongodb.initDb((err) => {
